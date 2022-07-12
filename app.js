bcfabd2735ed3263521a44dc03cda2d4e028cb4d
kosoewin <53328736+kosoewin@users.noreply.github.com>
2022-07-12 23:20:37 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-// For demonstration purposes only
-'use strict';
+const express = require('express');
+const app = express();
+const router = express.Router();
 
-var express = require('express'),
-    app = express();
+const path = __dirname + '/views/';
+const port = 8080;
 
-app.set('views', 'views');
-app.set('view engine', 'pug');
-
-app.get('/', function(req, res) {
-    res.render('home', {
-  });
+router.use(function (req,res,next) {
+  console.log('/' + req.method);
+  next();
 });
 
-app.listen(8080);
-module.exports.getApp = app;
+router.get('/', function(req,res){
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req,res){
+  res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path));
+app.use('/', router);
+
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!')
+})
