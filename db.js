ea711aa3e7a64b81717b68e30aec291fc3748570
kosoewin <53328736+kosoewin@users.noreply.github.com>
2022-08-06 15:35:14 +0630

    Create db.js

---
+++ db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const MONGO_USERNAME = 'docker';
+const MONGO_PASSWORD = 'docker';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017';
+const MONGO_DB = 'sharkinfo';
